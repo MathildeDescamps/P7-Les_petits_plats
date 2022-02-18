d93e08645b2044bd977f4c92f1c7f5460396adab
Mathilde Descamps <mdescamps@kira-factory.com>
2022-02-18 13:00:08 +0100

    We can now filter with tags (condition to fix)

--- app/js/RemiseAuClair.js
+++ app/js/RemiseAuClair.js
@@ -52,10 +52,146 @@ const displayResults = matches => {
                 </div>
             </div>
         `).join(' ');
-        matchList.innerText = html;
+        matchList.innerHTML = html;
     } else {
-        matchList.innerText = '';
+        matchList.innerHTML = '';
     }
+}
+//Filtrer les recettes avec les tags
+const filterWithTags = (recipesToFilter) => {
+    //On récupère les tags
+    taggedIngredientsDOM = Array.from(document.querySelectorAll('.searchbox__tags .ingredient-tags .tag .tag-text'));
+    taggedAppliancesDOM = Array.from(document.querySelectorAll('.searchbox__tags .appliance-tags .tag .tag-text'));
+    taggedUtensilsDOM = Array.from(document.querySelectorAll('.searchbox__tags .utensil-tags .tag .tag-text'));
+    let taggedIngredients = [];
+    let taggedAppliances = [];
+    let taggedUtensils = [];
+    taggedIngredientsDOM.forEach((taggedIngredient) => {
+        taggedIngredients.push(taggedIngredient.innerText);
+    })
+    taggedAppliancesDOM.forEach((taggedAppliance) => {
+        taggedAppliances.push(taggedAppliance.innerText);
+    })
+    taggedUtensilsDOM.forEach((taggedUtensil) => {
+        taggedUtensils.push(taggedUtensil.innerText);
+    })
+    console.log("tagged ingredient : ", taggedIngredients);
+    //On filtres les recettes d'après les tags
+    let matches = recipesToFilter.filter(recipe => {
+        let recipeIsMatching = false;
+        let ingredientsMatching = 0;
+        let appliancesMatching = 0;
+        let utensilsMatching = 0;
+        let ingredientsInTheRecipe = [];
+        let applianceInTheRecipe = [];
+        let utensilsInTheRecipe = [];
+        //On récupère tous les ingrédients de la recette
+        /* recipe.ingredients.forEach(({ingredient}) => {
+            ingredientsInTheRecipe.push(ingredient);
+        }) */
+        ingredientsInTheRecipe = recipe.ingredients.map(({ingredient}) => {
+            return ingredient;
+        })
+        console.log(ingredientsInTheRecipe);
+        //On récupère l'appareil de la recette
+        applianceInTheRecipe.push(recipe.appliance);
+        //On récupère tous les ustensils de la recette
+        recipe.utensils.forEach((utensil) => {
+            utensilsInTheRecipe.push(utensil);
+        })
+        console.log("Recipe : ");
+        //On filtre par ingrédient
+        console.log("Ingredients correspondants : ")
+        if(taggedIngredients.length > 0) {
+            taggedIngredients.forEach((taggedIngredient) => {
+                if(ingredientsInTheRecipe.includes(taggedIngredient)){
+                    console.log("La recette contient le tag : ", taggedIngredient);
+                    ingredientsMatching++;
+                }
+            })
+        }
+        //On filtre par appareil
+        console.log("Appareils correspondants : ");
+        if(taggedAppliances.length > 0){
+            taggedAppliances.forEach((taggedAppliance) => {
+                if(applianceInTheRecipe.includes(taggedAppliance)) {
+                    console.log("La recette contient le tag : ", taggedAppliance);
+                    appliancesMatching++;
+                }
+            })
+        }
+        //On filtre par ustensiles
+        console.log("Ustensiles correspondants : ")
+        if(taggedUtensils.length > 0) {
+            taggedUtensils.forEach((taggedUtensil) => {
+                if(utensilsInTheRecipe.includes(taggedUtensil)){
+                    console.log("La recette contient le tag : ", taggedUtensil);
+                    utensilsMatching++;
+                }
+            })
+        }
+        //Si tous les tags correspondent à la recette, alors on doit l'afficher
+        if((ingredientsMatching === taggedIngredients.length) /* && ((taggedAppliances.length > 0) && (appliancesMatching > 0)) && (utensilsMatching === taggedUtensils.length) */) {
+            recipeIsMatching = true;
+        }
+
+        return recipeIsMatching;
+
+    })
+    displayResults(matches);
+}
+
+//Supprimer un tag
+const deleteTag = (recipesToFilter) => {
+    //On écoute si l'utilisateur veut supprimer un tag
+    crosses = Array.from(document.querySelectorAll(".searchbox .searchbox__tags .tags-wrapper .tag .tag-icon"));
+    crosses.forEach((cross) => {
+        cross.addEventListener('click', function(e){
+            e.target.parentElement.remove();
+            filterWithTags(recipesToFilter);
+            search();
+        });
+    });
+    
+    //e.target.parentElement.remove();
+    filterWithTags(recipesToFilter);
+}
+//Ajouter un tag
+const addTag = (recipesToFilter, filterCategory, text, index) => {
+    //On crée le tag
+    let tag = document.createElement('div');
+    tag.classList.add('tag');
+    if(filterCategory === 'ingredient') {
+        tag.classList.add('bg-blue');
+        tag.classList.add('ingredient');
+        ingredientTagsWrapper.appendChild(tag);
+    } else if (filterCategory === 'utensil') {
+        tag.classList.add('bg-red');
+        tag.classList.add('utensil');
+        utensilTagsWrapper.appendChild(tag);
+    } else if (filterCategory === 'appliance') {
+        tag.classList.add('bg-green');
+        tag.classList.add('appliance');
+        applianceTagsWrapper.appendChild(tag);
+    }
+    tag.setAttribute('id', 'tag-'+index.toString());
+    //Le nom du tag
+    let tagText = document.createElement('p');
+    tagText.classList.add('tag-text');
+    tagText.innerText = text;
+    tag.appendChild(tagText);
+    //Et le bouton 'x' pour supprimer le tag
+    let icon = document.createElement('span');
+    icon.classList.add('tag-icon');
+    icon.classList.add('material-icons');
+    icon.classList.add('material-icons-outlined');
+    icon.innerText = 'highlight_off';
+    tag.appendChild(icon);
+
+    deleteTag(recipesToFilter);
+
+    //On refiltre les résultats selon le tag ajouté
+    filterWithTags(recipesToFilter);
 }
 // 0 : Pas de recherche, mais on peut ajouter et supprimer des tags
 const noSearchButTags = () => {
@@ -122,75 +258,6 @@ const noSearchButTags = () => {
             }
         })
     })
-}
-const filterWithTags = (recipesToFilter) => {
-    //On récupère les tags
-    taggedIngredients = Array.from(document.querySelectorAll('.searchbox__tags .ingredient-tags .tag .tag-text'));
-    taggedAppliances = Array.from(document.querySelectorAll('.searchbox__tags .appliance-tags .tag .tag-text'));
-    taggedUtensils = Array.from(document.querySelectorAll('.searchbox__tags .utensil-tags .tag .tag-text'));
-    console.log("1) ingrédients :")
-    taggedIngredients.forEach((taggedIngredient) => {
-        console.log(" -  ", taggedIngredient.innerText);
-    });
-    console.log("2) appareils :")
-    taggedAppliances.forEach((taggedAppliance) => {
-        console.log(" -  ", taggedAppliance.innerText);
-    });
-    console.log("3) ustensiles :")
-    taggedUtensils.forEach((taggedUtensil) => {
-        console.log(" -  ", taggedUtensil.innerText);
-    });
-}
-//Supprimer un tag
-const deleteTag = (recipesToFilter) => {
-    //On écoute si l'utilisateur veut supprimer un tag
-    crosses = Array.from(document.querySelectorAll(".searchbox .searchbox__tags .tags-wrapper .tag .tag-icon"));
-    crosses.forEach((cross) => {
-        cross.addEventListener('click', function(e){
-            e.target.parentElement.remove();
-            filterWithTags(recipesToFilter);
-        });
-    });
-    
-    //e.target.parentElement.remove();
-    filterWithTags(recipesToFilter);
-}
-//Ajouter un tag
-const addTag = (recipesToFilter, filterCategory, text, index) => {
-    //On crée le tag
-    let tag = document.createElement('div');
-    tag.classList.add('tag');
-    if(filterCategory === 'ingredient') {
-        tag.classList.add('bg-blue');
-        tag.classList.add('ingredient');
-        ingredientTagsWrapper.appendChild(tag);
-    } else if (filterCategory === 'utensil') {
-        tag.classList.add('bg-red');
-        tag.classList.add('utensil');
-        utensilTagsWrapper.appendChild(tag);
-    } else if (filterCategory === 'appliance') {
-        tag.classList.add('bg-green');
-        tag.classList.add('appliance');
-        applianceTagsWrapper.appendChild(tag);
-    }
-    tag.setAttribute('id', 'tag-'+index.toString());
-    //Le nom du tag
-    let tagText = document.createElement('p');
-    tagText.classList.add('tag-text');
-    tagText.innerText = text;
-    tag.appendChild(tagText);
-    //Et le bouton 'x' pour supprimer le tag
-    let icon = document.createElement('span');
-    icon.classList.add('tag-icon');
-    icon.classList.add('material-icons');
-    icon.classList.add('material-icons-outlined');
-    icon.innerText = 'highlight_off';
-    tag.appendChild(icon);
-
-    deleteTag(recipesToFilter);
-
-    //On refiltre les résultats selon le tag ajouté
-    filterWithTags(recipesToFilter);
 }
 //Au lancement de l'appli, on est dans le cas 0, donc on lance la fonction correspondante
 noSearchButTags();
